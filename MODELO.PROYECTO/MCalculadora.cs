40e14a4f2fb86fac56cac83f005ae93f7b645c4f
enkdress <felizjanuk12@gmail.com>
2020-10-27 20:03:51 -0500

    agregados los siguientes metodos al modelo de la calulcadora:
    Potencia
    ValorAbsoluto
    Factorial
    Serie1
    AleatorioFacil

--- MODELO.PROYECTO/MCalculadora.cs
+++ MODELO.PROYECTO/MCalculadora.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
 
 namespace MODELO.PROYECTO
 {
@@ -30,6 +31,48 @@ namespace MODELO.PROYECTO
             this._Number1 = n1;
             this._Number2 = n2;
             this._Resultado = r;
+        }
+
+        public MCalculadora(double n1, double n2)
+        {
+            this._Number1 = n1;
+            this._Number2 = n2;
+        }
+
+        public double Potencia() => Math.Pow(this._Number1, this._Number2);
+        public double ValorAbsoluto() => Math.Abs(this._Number2);
+        public double Factorial()
+        {
+            double counter = 1;
+            for(int i = 1; i <= this._Number1; i++ )
+            {
+                counter *= i;
+            }
+            return counter;
+        }
+        public String Serie1()
+        {
+            String serie = "";
+            for (int i = 1; i <= this._Number2; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    serie += " -" + i;
+                } else
+                {
+                    serie += i;
+                }
+            }
+
+            return serie;
+        }
+        public int AleatorioFacil()
+        {
+            int n1 = Int32.Parse(this._Number1.ToString());
+            int n2 = Int32.Parse(this._Number2.ToString());
+            Random rnd = new Random();
+
+            return rnd.Next(n1, n2);
         }
     }
 }
